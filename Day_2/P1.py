6616a3c453853ec6b8849b63de5c8de8eaae9d96
Anton <antel460@student.liu.se>
2023-12-02 06:23:19 +0100

    Day2, faster and a bit more clean than day 1

--- Day_2/P1.py
+++ Day_2/P1.py
@@ -1,6 +1,34 @@
 
 data = open("test.txt")
-#data = open("data.txt")
+data = open("data.txt")
 
+sum_ids = 0
 for line in data:
-    pass
+    line = line.strip("\n")
+    info = line.split(":")
+    id = int(info[0].split(" ")[1])
+
+    sets = info[1].split(";")
+    possible = True
+    for subset in sets:
+        green = 0
+        blue = 0
+        red = 0
+        cubes = subset.split(",")
+        for cube in cubes:
+            value = int(cube.split(" ")[1])
+            if "red" in cube:
+                red += value
+            elif "blue" in cube:
+                blue += value
+            elif "green" in cube:
+                green += value
+        if red > 12 or green > 13 or blue > 14:
+            possible = False
+    if possible:
+        print("id ", id)
+        sum_ids += id
+
+
+print(sum_ids)
+
--- Day_2/P2.py
+++ Day_2/P2.py
@@ -1,6 +1,30 @@
 
 data = open("test.txt")
-#data = open("data.txt")
+data = open("data.txt")
 
+sum_factor = 0
 for line in data:
-    pass
+    line = line.strip("\n")
+    info = line.split(":")
+    id = int(info[0].split(" ")[1])
+
+    sets = info[1].split(";")
+    greens = []
+    blues =  []
+    reds = []
+    for subset in sets:
+        cubes = subset.split(",")
+        for cube in cubes:
+            value = int(cube.split(" ")[1])
+            if "red" in cube:
+                reds.append(value)
+            elif "blue" in cube:
+                blues.append(value)
+            elif "green" in cube:
+                greens.append(value)
+    factor = max(reds)*max(greens)*max(blues)
+    sum_factor += factor
+
+
+print(sum_factor)
+
