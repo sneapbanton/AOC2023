1a1ca41d64f1ff80dcce1feff4a9a77664ef79ff
Anton <antel460@student.liu.se>
2023-12-06 06:29:03 +0100

    Day 6 done, struggled with input due to just have woken up, could have been a lot quicker

--- Day_6/P1.py
+++ Day_6/P1.py
@@ -1,6 +1,29 @@
 
 data = open("test.txt")
-#data = open("data.txt")
+data = open("data.txt")
+
+factor = 1
+
+times = []
+dists = []
+
+rows = []
 
 for line in data:
-    pass
+    line = line.strip("\n")
+    rows.append(line)
+
+times = [int(x) for x in rows[0].split(" ") if "Time:" in rows[0] and x != "" and x.isnumeric()]
+dists = [int(x) for x in rows[1].split(" ") if "Distance:" in rows[1] and x != "" and x.isnumeric()]
+
+for i in range(0, len(times)):
+    ways = 0
+    time = times[i]
+    for w in range(time):
+        total_dist = w * (time - w)
+        if (total_dist > dists[i]):
+            ways += 1
+    if ways:
+        factor = factor * ways
+
+print(factor)
--- Day_6/P2.py
+++ Day_6/P2.py
@@ -1,6 +1,35 @@
 
 data = open("test.txt")
-#data = open("data.txt")
+data = open("data.txt")
+
+factor = 1
+
+times = []
+dists = []
+
+rows = []
 
 for line in data:
-    pass
+    line = line.strip("\n")
+    rows.append(line)
+
+times = [x for x in rows[0].split(" ") if "Time:" in rows[0] and x != "" and x.isnumeric()]
+dists = [x for x in rows[1].split(" ") if "Distance:" in rows[1] and x != "" and x.isnumeric()]
+
+time = ""
+dist = ""
+for i in range(len(times)):
+    time += times[i]
+    dist += dists[i]
+
+time = int(time)
+dist = int(dist)
+
+ways = 0
+for w in range(time):
+    total_dist = w * (time - w)
+    if (total_dist > dist):
+        ways += 1
+factor = factor * ways
+
+print(factor)
