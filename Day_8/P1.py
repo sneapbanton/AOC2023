8b6b03afee0ff5fe5207bc6b4112082a8e43a4d0
Anton <antel460@student.liu.se>
2023-12-09 17:13:14 +0100

    Day 8

--- Day_8/P1.py
+++ Day_8/P1.py
@@ -1,6 +1,36 @@
 
 data = open("test.txt")
-#data = open("data.txt")
+data = open("data.txt")
 
+path = dict()
+instructions = []
 for line in data:
-    pass
+    line = line.strip("\n")
+
+    if line and "=" in line:
+        key, values = line.split("=")
+        key = key[:-1]
+        left, right = map(str, values[2:-1].split(","))
+        right = right[1:]
+        path[key] = (left, right)
+
+    elif line:
+        instructions = list(line)
+
+current_node = "AAA"
+steps = 0
+instr = 0
+while current_node != "ZZZ":
+    curr_instr = instructions[instr]
+    if curr_instr == "L":
+        current_node = path[current_node][0]
+    elif curr_instr == "R":
+        current_node = path[current_node][1]
+    else:
+        assert False
+    steps += 1
+    instr += 1
+    if instr >= len(instructions):
+        instr = 0
+
+print(steps) 
--- Day_8/P2.py
+++ Day_8/P2.py
@@ -1,6 +1,54 @@
 
-data = open("test.txt")
-#data = open("data.txt")
+import math
 
+data = open("test.txt")
+data = open("data.txt")
+
+path = dict()
+instructions = []
+starting_nodes = []
 for line in data:
-    pass
+    line = line.strip("\n")
+
+    if line and "=" in line:
+        key, values = line.split("=")
+        key = key[:-1]
+        left, right = map(str, values[2:-1].split(","))
+        right = right[1:]
+        path[key] = (left, right)
+        if key[-1] == "A":
+            starting_nodes.append(key)
+
+    elif line:
+        instructions = list(line)
+
+print(len(starting_nodes))
+
+def prime_def():
+    return 0
+
+s = []
+for i in range(len(starting_nodes)):
+    steps = 0
+    instr = 0
+    current_node = starting_nodes[i]
+    old_len_set = -1
+    while True:
+        curr_instr = instructions[instr]
+        if curr_instr == "L":
+            current_node = path[current_node][0]
+        elif curr_instr == "R":
+            current_node = path[current_node][1]
+        else:
+            assert False
+
+        steps += 1
+        instr = (instr + 1) % len(instructions)
+        if (current_node[-1] == "Z"):
+            print(steps)
+            s.append(steps)
+            break
+    print()
+
+
+print(math.lcm(*s))
