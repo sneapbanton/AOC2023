67d0a2363ed345a86c7e74df1e0c53fe78839172
Anton <antel460@student.liu.se>
2023-12-10 22:24:28 +0100

    Day 9

--- Day_9/P2.py
+++ Day_9/P2.py
@@ -1,6 +1,25 @@
 
 data = open("test.txt")
-#data = open("data.txt")
+data = open("data.txt")
+
+
+def extrapolate(nums):
+    new_nums = []
+    for i in range(len(nums)-1):
+        new_nums.append(int(nums[i+1]) - int(nums[i]))
+    if set(new_nums) == set([0]):
+        return 0
+    new_num = extrapolate(new_nums)
+    return new_nums[-1] + new_num
+
+
+summary_extrapolate = 0
 
 for line in data:
-    pass
+    line = line.strip("\n")
+    nums = line.split(" ")
+    nums.reverse()
+    extra_num = extrapolate(nums)
+    summary_extrapolate += int(nums[-1]) + extra_num
+
+print(summary_extrapolate)
