144e0132dd45576ff59948e5fbd0365b67b27ffa
Anton <antel460@student.liu.se>
2023-12-12 22:29:17 +0100

    Day 11 done

--- Day_11/P1.py
+++ Day_11/P1.py
@@ -1,6 +1,75 @@
+import numpy as np
+import tqdm
 
 data = open("test.txt")
-#data = open("data.txt")
+data = open("data.txt")
+
+space = []
 
 for line in data:
-    pass
+    line = line.strip("\n")
+    line = list(line)
+    space.append(line)
+
+space = np.array(space)
+rows, cols = space.shape
+
+r = 0
+while r < rows:
+    if len(set(space[r,:])) == 1:
+        space = np.insert(space, r, ".", axis=0)
+        rows += 1
+        r += 2
+    else:
+        r += 1
+
+c = 0
+while c < cols:
+    if len(set(space[:,c])) == 1:
+        space = np.insert(space, c, ".", axis=1)
+        c += 1
+        cols += 1
+    c += 1
+
+
+nodes = []
+for r in range(rows):
+    for c in range(cols):
+        if space[r,c] == "#":
+            nodes.append((r,c))
+
+from collections import defaultdict
+import math, heapq
+def dijsktra(start):
+    q = []
+    costs = defaultdict(lambda: math.inf)
+    prev = defaultdict(lambda: -1)
+    costs[start] = 0
+    heapq.heappush(q, (0, start))
+
+    while q:
+        cost, node = heapq.heappop(q)
+        
+        r, c = node
+        if (0<=r<rows and 0<=c<cols):
+            neighs = [(r-1,c),(r+1,c),(r,c-1),(r,c+1)]
+            neighs = [neigh for neigh in neighs if costs[neigh] > cost+1]
+            for neigh in neighs:
+                costs[neigh] = cost+1
+                prev[neigh] = node
+                heapq.heappush(q, (cost+1, neigh))
+    
+    return costs
+
+all_costs = []
+for i in tqdm.tqdm(range(len(nodes))):
+    node = nodes[i]
+    costs = dijsktra(node)
+    all_costs.append(costs)
+
+result = 0
+for i in range(len(nodes)):
+    for j in range(i, len(nodes)):
+        if i != j:
+            result += all_costs[i][nodes[j]]
+print(result)
--- Day_11/P2.py
+++ Day_11/P2.py
@@ -1,6 +1,79 @@
+import numpy as np
 
-data = open("test.txt")
-#data = open("data.txt")
+def B3(dataset, n):
+    space = []
 
-for line in data:
-    pass
+    data = dataset
+
+    for line in data:
+        line = line.strip("\n")
+        line = list(line)
+        space.append(line)
+
+    space = np.array(space)
+    rows, cols = space.shape
+    
+
+    r = 0
+    while r < rows:
+        if len(set(space[r,:])) == 1:
+            space = np.delete(space, r, axis=0)
+            space = np.insert(space, r, "-", axis=0)
+        r += 1
+
+    c = 0
+    while c < cols:
+        if set(space[:,c]) == set([".", "-"]):
+            space = np.delete(space, c, axis=1)
+            space = np.insert(space, c, "|", axis=1)
+        c += 1
+
+
+    nodes = []
+    for r in range(rows):
+        for c in range(cols):
+            if space[r,c] == "#":
+                nodes.append((r,c))
+            elif space[r,c] == "|" and 0<r<rows-1 and 0<c<cols-1 and space[r-1,c]==space[r+1,c]=="|" and space[r,c-1]==space[r,c+1]=="-":
+                space[r,c] = "+"
+
+    def traverser(start, goal, n):
+        sr, sc = start
+        gr, gc = goal
+        r = sr
+        c = sc
+        steps = 0
+        step_r = int((gr-sr) / max(1, abs(gr-sr)))
+        step_c = int((gc-sc) / max(1, abs(gc-sc)))
+        
+        while r != gr:
+            if space[r,c] == "-":
+                steps += n
+            else:
+                steps += 1
+            r += step_r
+        while c != gc:
+            if space[r,c] == "|":
+                steps += n
+            else:
+                steps += 1
+            c += step_c
+        return steps
+        
+
+    result = 0
+    for i in range(len(nodes)):
+        for j in range(i, len(nodes)):
+            if (i != j):
+                result += traverser(nodes[i], nodes[j], n)
+
+    return result
+
+
+if __name__=="__main__":
+    test_data = open("test.txt")
+    data = open("data.txt")
+    assert(B3(test_data, 10) == 1030)
+    test_data = open("test.txt")
+    assert(B3(test_data, 100) == 8410)
+    print(B3(data, 1000000))
