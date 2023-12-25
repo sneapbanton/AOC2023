fafd903648adc8e44fcb4b02d738c4f9f4b31928
Anton <antel460@student.liu.se>
2023-12-25 12:48:52 +0100

    Day 25

--- Day_25/P1.py
+++ Day_25/P1.py
@@ -1,6 +1,18 @@
+from networkx import Graph, minimum_cut
+from itertools import combinations
 
 data = open("test.txt")
-#data = open("data.txt")
+data = open("data.txt")
 
+groph = Graph()
 for line in data:
-    pass
+    key, values = line.strip("\n").split(":")
+    values = values.split(" ")
+    for val in values[1:]:
+        groph.add_edge(key,val,capacity=1)
+
+for start,end in combinations(groph.nodes, 2):
+    cut, parts = minimum_cut(groph, start, end)
+    if cut == 3:
+        print(len(parts[0])*len(parts[1]))
+        break
